7cc992d90a3a0bf08b1512b44bada015947e79b7
DottoXD <80960175+DottoXD@users.noreply.github.com>
2026-05-30 08:55:03 +0200

    Fix

--- matugen.sh
+++ matugen.sh
@@ -1,3 +1,3 @@
-matugen image /home/daniel/Immagini/wallpaper.png -t scheme-tonal-spot -c ~/.config/matugen/config.toml
+matugen image ~/.config/Immagini/wallpaper.png -t scheme-tonal-spot -c ~/.config/matugen/config.toml
 sed -i 's/, /,/g' ~/.config/hypr/colors.conf
 cp ~/.config/gtk-4.0/gtk.css ~/.config/gtk-3.0/gtk.css
